da7ec809665440b5e057dce59d20560886c2cbea
Rashi Shukla <rashi.shukl1323@gmail.com>
2026-09-06 20:35:13 +0530

    Setting up basics

---
+++ conditional/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+    
+    public static void main(String[] args) {
+        /*
+        Syntax of if statements:
+        if (boolean expression T or F){
+        
+         */
+
+        int x = 1000;
+        if (x > 100){
+            x = x + 200;
+        }else {
+            x = x + 100;
+        }
+
+        System.out.println(x);
+        
+    }
+
+}
---
+++ javaBasics/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("hiii");
+        Scanner input = new Scanner(System.in);
+        System.out.print(input.nextInt());
+    }
+
+}
+//public - class can be accesed from anywhwere
+//Main - NAME OF FUNCTION
+//Main - entry point of javacode
