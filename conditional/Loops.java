545eb03a21c4742d3b11754be2788f13ee694d96
Rashi Shukla <rashi.shukl1323@gmail.com>
2026-09-06 21:37:28 +0530

    Largest number problem

--- conditional/Loops.java
+++ conditional/Loops.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class Loops{
     public static void main(String[] args){
         /*
+        for loop
         syntax for (intialisation , condition , increment/ decrement){
         //body
         }
          */
+        /*
         //Q = print numbers from 1 to 5
-        for ( int num = 1 ; num <= 5 ; num++){
+        for ( int num = 1 ; num <= 5 ; num +=2){
             System.out.println(num);
+        }
+
+        // print numbers from 1 to n
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int num = 1 ; num <= n ; num +=1){
+            System.out.println(num);
+        }s */
+
+        /*
+        while loops
+        while(condition){
+            ///body
+        }
+         */
+        int num = 1;
+        while (num <=5){
+            System.out.println(num);
+            num++;
         }
     }
 }
